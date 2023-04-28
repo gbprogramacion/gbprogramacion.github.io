81d7ae1d4f11658b75013ed94e9d5ab31db7af27
gbprogramacion <75961382+gbprogramacion@users.noreply.github.com>
2023-04-28 13:37:48 -0500

    initial commit

---
+++ JS/btn__galeria.js
@@ -0,0 +1,24 @@
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+menu = document.getElementById("header");
+body = document.getElementById("container__all__galeria");
+nav = document.getElementById("nav");
+
+function mostrar_menu(){
+
+    menu.classList.toggle('move_content');
+    body.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+}
+
+window.addEventListener("resize", function(){
+
+    if(window.innerWidth > 760){
+
+    menu.classList.remove('move_content');
+    body.classList.remove('move_content');
+    nav.classList.remove('move_nav');
+
+    }
+
+})
